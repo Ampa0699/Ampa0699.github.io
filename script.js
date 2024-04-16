ad9fd5996e856e2c75a423974915d6912cb8f259
Ampa <ampamohammad@gmail.com>
2024-04-17 06:20:30 +0800

    added

--- script.js
+++ script.js
@@ -51,6 +51,21 @@ ScrollReveal({
 ScrollReveal().reveal(".home-content,.heading", {
   origin: "top",
 });
+ScrollReveal().reveal(
+  ".home-img,.services-container, .portfolio-box, .contact form",
+  {
+    origin: "bottom",
+  }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", {
+  origin: "left",
+});
+ScrollReveal().reveal(
+  ".home-content p, .about-content,.about-personal-info,.about-skill,.about-education,.about-experience",
+  {
+    origin: "right",
+  }
+);
 
 // scroll reveal end
 const typed = new Typed(".multiple-text", {
